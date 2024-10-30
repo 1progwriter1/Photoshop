aecb50963ed6e9b4068b6946ffd86f9d60569801
Иван Водолаженко <vig130205@mail.ru>
2024-10-30 14:40:15 +0300

    canvas impl

--- headers/api_impl/canvas.hpp
+++ headers/api_impl/canvas.hpp
@@ -2,7 +2,61 @@
 #define PHOTOSHOP_CANVAS_IMPLEMENTATION
 
 
+#include <api_canvas.hpp>
 
+
+using namespace psapi;
+
+using sfm::Color;
+
+
+
+class Layer : public ILayer
+{
+    vec2i pos_;
+    vec2i size_;
+
+    std::vector<Color> pixels_;
+public:
+    Layer( vec2i size, vec2i pos = vec2i());
+    ~Layer() = default;
+
+    Color getPixel(sfm::vec2i pos) const override;
+    void setPixel(sfm::vec2i pos, sfm::Color pixel) override;
+};
+
+
+class Canvas : public ICanvas
+{
+    std::vector<std::unique_ptr<ILayer>> layers_;
+public:
+    Canvas() = default;
+    ~Canvas() = default;
+
+    ILayer*       getLayer(size_t index)       override;
+    const ILayer* getLayer(size_t index) const override;
+
+    ILayer*       getTempLayer()       override;
+    const ILayer* getTempLayer() const override;
+
+    void cleanTempLayer() override;
+
+    size_t getNumLayers() const override;
+
+    size_t getActiveLayerIndex()       const override;
+    void   setActiveLayerIndex(size_t index) override;
+
+    bool insertLayer     (size_t index, std::unique_ptr<ILayer> layer) override;
+    bool removeLayer     (size_t index) override;
+    bool insertEmptyLayer(size_t index) override;
+
+    void setPos  (sfm::vec2i pos)   override;
+    void setSize (sfm::vec2i size)  override;
+    void setScale(sfm::vec2f scale) override;
+
+    sfm::vec2i getMousePosition() const override;
+    bool isPressed()              const override;
+};
 
 
 #endif // PHOTOSHOP_CANVAS_IMPLEMENTATION
--- src/api_impl/canvas.cpp
+++ src/api_impl/canvas.cpp
@@ -1,17 +1,138 @@
 #include <api_impl/canvas.hpp>
 #include <api_canvas.hpp>
+#include <cassert>
 
 
-using namespace psapi;
+Layer::Layer( vec2i size, vec2i pos /*= vec2i()*/)
+    :   size_( size), pos_( pos), pixels_( size.x * size.y, Color()) {}
 
 
-class ALayer : public ILayer
+Color Layer::getPixel(sfm::vec2i pos) const
 {
+    return pixels_[pos.y * size_.x + pos.x];
+}
 
-};
 
-
-class ACanvas : public ICanvas
+void Layer::setPixel(sfm::vec2i pos, sfm::Color pixel)
 {
+    pixels_[pos.y * size_.x + pos.x] = pixel;
+}
 
-};
+
+ILayer* Canvas::getLayer(size_t index)
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+const ILayer* Canvas::getLayer(size_t index) const
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+ILayer* Canvas::getTempLayer()
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+const ILayer* Canvas::getTempLayer() const
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+void Canvas::cleanTempLayer()
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+size_t Canvas::getNumLayers() const
+{
+    assert( 0 && "Not implemented" );
+
+    return 0;
+}
+
+
+size_t Canvas::getActiveLayerIndex() const
+{
+    assert( 0 && "Not implemented" );
+
+    return 0;
+}
+
+
+void Canvas::setActiveLayerIndex(size_t index)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+bool Canvas::insertLayer(size_t index, std::unique_ptr<ILayer> layer)
+{
+    assert( 0 && "Not implemented" );
+
+    return true;
+}
+
+
+bool Canvas::removeLayer(size_t index)
+{
+    assert( 0 && "Not implemented" );
+
+    return true;
+}
+
+
+bool Canvas::insertEmptyLayer(size_t index)
+{
+    assert( 0 && "Not implemented" );
+
+    return true;
+}
+
+
+void Canvas::setPos(sfm::vec2i pos)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void Canvas::setSize(sfm::vec2i size)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void Canvas::setScale(sfm::vec2f scale)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+sfm::vec2i Canvas::getMousePosition() const
+{
+    assert( 0 && "Not implemented" );
+
+    return vec2i();
+}
+
+
+bool Canvas::isPressed() const
+{
+    assert( 0 && "Not implemented" );
+
+    return false;
+}
+
