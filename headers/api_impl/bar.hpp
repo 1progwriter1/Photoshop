6be76f0426ae24a12f3a878120fc8d493dc233e7
Иван Водолаженко <vig130205@mail.ru>
2024-10-30 15:00:07 +0300

    bar impl

--- headers/api_impl/bar.hpp
+++ headers/api_impl/bar.hpp
@@ -5,9 +5,52 @@
 #include <api_bar.hpp>
 
 
-class ABarButton : public psapi::IBarButton
-{
+using namespace psapi;
 
+
+class ABarButton : public IBarButton
+{
+public:
+    virtual ~ABarButton() = default;
+
+    virtual void draw(IRenderWindow* renderWindow) override;
+    virtual bool update(const IRenderWindow* renderWindow, const Event& event) override;
+    virtual wid_t getId() const override;
+    virtual IWindow* getWindowById(wid_t id) override;
+    virtual const IWindow* getWindowById(wid_t id) const override;
+    virtual vec2i getPos() const override;
+    virtual vec2u getSize() const override;
+    virtual void setParent(const IWindow* parent) override;
+    virtual void forceActivate() override;
+    virtual void forceDeactivate() override;
+    virtual bool isActive() const override;
+    virtual bool isWindowContainer() const override;
+
+    virtual void setState(State state) override;
+    virtual State getState() const override;
+};
+
+
+class ABar : public IBar
+{
+public:
+    virtual ~ABar() = default;
+
+    virtual void draw(IRenderWindow* renderWindow) override;
+    virtual bool update(const IRenderWindow* renderWindow, const Event& event) override;
+    virtual wid_t getId() const override;
+    virtual IWindow* getWindowById(wid_t id) override;
+    virtual const IWindow* getWindowById(wid_t id) const override;
+    virtual vec2i getPos() const override;
+    virtual vec2u getSize() const override;
+    virtual void setParent(const IWindow* parent) override;
+    virtual void forceActivate() override;
+    virtual void forceDeactivate() override;
+    virtual bool isActive() const override;
+    virtual bool isWindowContainer() const override;
+
+    virtual ChildInfo getNextChildInfo() const override;
+    virtual void finishButtonDraw(IRenderWindow* renderWindow, const IBarButton* button) const override;
 };
 
 
--- src/api_impl/bar.cpp
+++ src/api_impl/bar.cpp
@@ -1,23 +1,206 @@
 #include <api_impl/bar.hpp>
+#include <cassert>
 
 
-// class ABar : public psapi::IBar
-// {
-// public:
-//     virtual psapi::vec2i getNextChildPos()  const = 0;
-//     virtual psapi::vec2i getNextChildSize() const = 0;
-//
-//     virtual void finishButtonDraw( psapi::sfm::ARenderWindow* renderWindow, const psapi::IBarButton *button) = 0;
-//
-//     virtual psapi::vec2i getPos() const = 0;
-//     virtual psapi::vec2i getSize() const = 0;
-//
-//
-// };
-//
-//
-// class ABarButton : public psapi::IBarButton
-// {
-// public:
-//     virtual State state() = 0;
-// };
+void ABarButton::draw(IRenderWindow* renderWindow)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+bool ABarButton::update(const IRenderWindow* renderWindow, const Event& event)
+{
+    assert( 0 && "Not implemented" );
+
+    return true;
+}
+
+
+wid_t ABarButton::getId() const
+{
+    assert( 0 && "Not implemented" );
+
+    return 0;
+}
+
+
+IWindow* ABarButton::getWindowById(wid_t id)
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+const IWindow* ABarButton::getWindowById(wid_t id) const
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+vec2i ABarButton::getPos() const
+{
+    assert( 0 && "Not implemented" );
+
+    return vec2i();
+}
+
+
+vec2u ABarButton::getSize() const
+{
+    assert( 0 && "Not implemented" );
+
+    return vec2u();
+}
+
+
+void ABarButton::setParent(const IWindow* parent)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void ABarButton::forceActivate()
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void ABarButton::forceDeactivate()
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+bool ABarButton::isActive() const
+{
+    assert( 0 && "Not implemented" );
+
+    return false;
+}
+
+
+bool ABarButton::isWindowContainer() const
+{
+    assert( 0 && "Not implemented" );
+
+    return false;
+}
+
+
+void ABarButton::setState(State state)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+IBarButton::State ABarButton::getState() const
+{
+    assert( 0 && "Not implemented" );
+
+    return IBarButton::State::Normal;
+}
+
+
+void ABar::draw(IRenderWindow* renderWindow)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+bool ABar::update(const IRenderWindow* renderWindow, const Event& event)
+{
+    assert( 0 && "Not implemented" );
+
+    return true;
+}
+
+
+wid_t ABar::getId() const
+{
+    assert( 0 && "Not implemented" );
+
+    return 0;
+}
+
+
+IWindow* ABar::getWindowById(wid_t id)
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+const IWindow* ABar::getWindowById(wid_t id) const
+{
+    assert( 0 && "Not implemented" );
+
+    return nullptr;
+}
+
+
+vec2i ABar::getPos() const
+{
+    assert( 0 && "Not implemented" );
+
+    return vec2i();
+}
+
+
+vec2u ABar::getSize() const
+{
+    assert( 0 && "Not implemented" );
+
+    return vec2u();
+}
+
+
+void ABar::setParent(const IWindow* parent)
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void ABar::forceActivate()
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+void ABar::forceDeactivate()
+{
+    assert( 0 && "Not implemented" );
+}
+
+
+bool ABar::isActive() const
+{
+    assert( 0 && "Not implemented" );
+
+    return false;
+}
+
+
+bool ABar::isWindowContainer() const
+{
+    assert( 0 && "Not implemented" );
+
+    return false;
+}
+
+
+ChildInfo ABar::getNextChildInfo() const
+{
+    assert( 0 && "Not implemented" );
+
+    return ChildInfo();
+}
+
+
+void ABar::finishButtonDraw(IRenderWindow* renderWindow, const IBarButton* button) const
+{
+    assert( 0 && "Not implemented" );
+}
